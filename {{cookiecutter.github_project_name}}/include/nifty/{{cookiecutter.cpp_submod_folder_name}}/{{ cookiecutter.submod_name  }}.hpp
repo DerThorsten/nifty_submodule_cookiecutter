dbaea90d92cce2795e71617d44d76a11bdeec0de
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-09-11 10:51:17 +0200

    inital commit

---
+++ {{cookiecutter.github_project_name}}/include/nifty/{{cookiecutter.cpp_submod_folder_name}}/{{ cookiecutter.submod_name  }}.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "xtensor/xtensor.hpp"
+#include "xtensor/xarray.hpp"
+#include "xtensor/xexpression.hpp"
+
+
+namespace nifty{
+namespace {{ cookiecutter.cpp_submod_namespace }} {
+
+
+    bool helloWorld(){
+        return true;
+    }
+
+} // end namespace {{cookiecutter.cpp_submod_namespace}}
+}
