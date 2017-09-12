a9663082d0a81ef86eee797dd13b745be75d4e2e
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-09-12 11:42:40 +0200

    minor changes

--- {{cookiecutter.github_project_name}}/src/examples/example_1.cpp
+++ {{cookiecutter.github_project_name}}/src/examples/example_1.cpp
@@ -1,7 +1,6 @@
 #include "nifty/{{cookiecutter.cpp_submod_folder_name}}/{{ cookiecutter.submod_name  }}.hpp"
 
 
-
 int main(){
     auto res = nifty::{{cookiecutter.cpp_submod_namespace}}::hello_world();
 }
